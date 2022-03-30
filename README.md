870e51b60ce47d05d56e9ce82f07759fd702882c
Ryan Giordano <rgiorda1@gmail.com>
2022-03-30 05:39:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+<p align="center">
+  Phaser 3 Boilerplate with Utilities.
+</p>
+
+<div align="center">
+
+  ![Dependencies](https://img.shields.io/david/digitsensitive/phaser3-typescript.svg)
+  ![GitHub issues](https://img.shields.io/github/issues/digitsensitive/phaser3-typescript.svg)
+  ![GitHub stars](https://img.shields.io/github/stars/digitsensitive/phaser3-typescript.svg)
+  ![Contributions welcome](https://img.shields.io/badge/contributions-welcome-orange.svg)
+  ![Code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)
+  ![GitHub licence](https://img.shields.io/github/license/digitsensitive/phaser3-typescript.svg)
+
+</div>
+
+# Phaser 3 and TypeScript
+
+[Phaser](https://github.com/photonstorm/phaser) is a wonderful, fast and
+free open source HTML5 game framework.
+Here you will find everything you need to develop games with Phaser 3
+in TypeScript. Have fun discovering, developing and playing!
+Ideas and corrections are highly desirable to constantly
+improve this repository!
+
+## Getting started
+
+### Prerequisites
+
+```
+Download and install npm with Node.js @ https://nodejs.org/en
+```
+
+### Installing
+
+Select a folder, navigate to it, and clone this repository
+with this command-line:
+
+```
+git clone https://github.com/digitsensitive/phaser3-typescript.git
+```
+
+Install the dependencies with this command-line:
+
+```
+npm install
+```
+
+### Building and Running
+
+Perform a quick build (bundle.js) and start server:
+
+```
+npm start
+```
+
+## External Resources
+
+- [Phaser 3 Framework](https://github.com/photonstorm/phaser)
+- [Phaser 3 Docs with TypeScript Definition File](https://github.com/photonstorm/phaser3-docs)
+- [Phaser 3 Online Docs](https://photonstorm.github.io/phaser3-docs/index.html)
+- [Phaser 3 Official Examples](https://github.com/photonstorm/phaser3-examples)
+- [Phaser 3 Discourse](https://phaser.discourse.group)
+
+## Helpful tools
+
+- [Leshy SpriteSheet Tool](https://www.leshylabs.com/apps/sstool)
+- [Tiled](https://www.mapeditor.org)
+- [Littera](http://kvazars.com/littera)
+- [Tile Extruder](https://github.com/sporadic-labs/tile-extruder)
+
+## TypeScript Configuration
+
+### tsconfig.json
+
+The following `Compiler Options` have been set in the `tsconfig.json` file:
+
+| Option | Value     |
+| :------------- | :------------- |
+| target       | ES2016 |
+| module       | CommonJS |
+| sourceMap    | true |
+| noImplicitAny| true [WIP] |
+| strict       | true [WIP] |
+
+You can see the complete list of the available options at [here](https://www.typescriptlang.org/docs/handbook/tsconfig-json.html).
+Interesting read about [setting up a nodejs-project](https://codeburst.io/tips-for-setting-up-a-typescript-nodejs-project-5d1c48dc1a2d).
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/digitsensitive/phaser3-typescript/blob/master/LICENSE) file for details.
---
+++ src/scenes/bootScene.ts
@@ -0,0 +1,115 @@
+import { AnimationHelper } from "../utility/tweens/animation-helper";
+import { BLACK, WHITE } from "../utility/Constants";
+import { State } from "../utility/state/State";
+import { MainScene } from "./MainScene";
+import DependentScene from "./DependentScene";
+
+function preloadSceneDependencies(
+  bootScene: Phaser.Scene,
+  scenes: typeof DependentScene[]
+) {
+  scenes.forEach((scene) => {
+    const { spriteDependencies, audioDependencies } = scene;
+    if (spriteDependencies?.length) {
+      spriteDependencies.forEach((sd) => {
+        bootScene.load.spritesheet(sd.key, sd.url, {
+          frameWidth: sd.frameWidth,
+          frameHeight: sd.frameHeight,
+        });
+      });
+    }
+    if (audioDependencies?.length) {
+      audioDependencies.forEach((ad) => {
+        bootScene.load.audio(ad.key, ad.url);
+      });
+    }
+  });
+}
+
+export class BootScene extends Phaser.Scene {
+  private loadingBar: Phaser.GameObjects.Graphics;
+  private progressBar: Phaser.GameObjects.Graphics;
+  private loaded: boolean = false;
+  constructor() {
+    super({ key: "BootScene" });
+  }
+
+  public init() {
+    if (this.loaded) {
+      this.runStartupProcess();
+    }
+  }
+  private runStartupProcess() {
+    const animationHelper = new AnimationHelper(this);
+    animationHelper.createGameAnimations(
+      this.cache.json.get("ryanAndLoAnimation").anims
+    );
+
+    const sprite = this.add.sprite(400, 300, "ryanandlo");
+    sprite.scaleX = 1;
+    sprite.scaleY = 1;
+    sprite.anims.play("shine-in");
+    sprite.on("animationcomplete", () => {
+      this.sound.play("startup", { volume: 0.1 });
+
+      this.add.text(300, 330, "Catshape Daruma®", {
+        fontFamily: "pixel",
+        fontSize: "20px",
+        color: BLACK.str,
+        fontStyle: "strong",
+      });
+
+      setTimeout(() => {
+        this.scene.start("Audio");
+        this.scene.start("MainScene");
+      }, 1);
+    });
+
+    // When we get to the point  where we can save state to a JSON, this is where we'd load it in, flipping the proper flags.
+    const sm = State.getInstance();
+    sm.initialize(this.game);
+  }
+
+  preload(): void {
+    // this.sound.add("startup");
+    this.cameras.main.setBackgroundColor(WHITE.hex);
+    this.createLoadingGraphics();
+    this.load.on("complete", () => {
+      this.loaded = true;
+      this.runStartupProcess();
+    });
+    // Load the packages
+    this.load.pack(
+      "preload_spritesheets",
+      "./src/assets/pack/spritesheets.json",
+      "preload_spritesheets"
+    );
+    this.load.pack(
+      "preload_images",
+      "./src/assets/pack/image.json",
+      "preload_images"
+    );
+    this.load.pack(
+      "preload_audio",
+      "./src/assets/pack/audio.json",
+      "preload_audio"
+    );
+    this.load.pack(
+      "preload_data",
+      "./src/assets/pack/data.json",
+      "preload_data"
+    );
+    this.load.pack(
+      "preload_tilemaps",
+      "./src/assets/pack/tilemaps.json",
+      "preload_tilemaps"
+    );
+
+    this.load.pack("preload", "./src/assets/pack.json", "preload");
+
+    preloadSceneDependencies(this, [MainScene]);
+  }
+  private createLoadingGraphics(): void {
+    // We can specify the type of config we want to send.
+  }
+}
