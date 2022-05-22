aa774c4926128a760eeefc2f70f32c71f7027ad9
thedaruma <rgiorda1@gmail.com>
2022-05-22 05:36:17 -0400

    add state as a separate concept from the scene

--- src/components/state/PlayerState.ts
+++ src/components/state/PlayerState.ts
@@ -1,3 +1,6 @@
+//TODO: Not in use. Replace logic in StateScene with this 
+// so we can unit test it.
+
 import { ResourceType } from "../../assets/data/stellar-bodies/Types";
 
 export type StateResourceObject = {
@@ -19,7 +22,8 @@ const defaultState: PlayerStateMap = new Map([
 
 export function createPlayerState(
   initialState = defaultState,
-  onResourceChange: (state: PlayerStateMap) => void
+  onResourceChange: ({ key: ResourceType, resource: number }) => void,
+  onResourceMaxValueChange: ({ key: ResourceType, newMaxValue: number }) => void
 ) {
   const state = new Map(initialState);
 
@@ -27,12 +31,58 @@ export function createPlayerState(
     return state;
   }
 
-  function setResource(key: ResourceType, value: number): PlayerStateMap {
-    const resource = state.get(key);
-    resource.current = value;
-    state.set(key, resource);
+  function incrementResource(key: ResourceType, valueToAdd: number) {
+    setResource(key, valueToAdd + state.get(key).current);
+  }
 
-    onResourceChange(state);
-    return state;
+  function decrementResource(key: ResourceType, valueToSubtract: number) {
+    setResource(key, state.get(key).current - valueToSubtract);
+  }
+
+  function setResource(key: ResourceType, value: number) {
+    assertValue(key);
+    state.get(key).current = Math.max(0, Math.min(value, state.get(key).max));
+    // this.game.events.emit("resource-value-change", {
+    //   key,
+    //   resource: this[key],
+    // });
+    onResourceChange({
+      key,
+      resource: state.get(key),
+    });
+  }
+
+  function setResourceMaxValue(resource: ResourceType, newMaxValue: number) {
+    if (newMaxValue <= 0) {
+      throw new Error(`New maxValue for Resource cannot be ${newMaxValue}`);
+    }
+    // this.game.events.emit("resource-max-value-change", {
+    //   key: resource,
+    //   newMaxValue,
+    // });
+    onResourceMaxValueChange({ key: resource, newMaxValue });
+  }
+
+  function getValue(key: string): number {
+    this.assertValue(key);
+    return this[key].current;
+  }
+
+  function getResource(resourceType: ResourceType) {
+    return this[resourceType];
+  }
+
+  function assertValue(key: string) {
+    if (this[key] === undefined) {
+      throw new Error(`state property ${key} does not exist`);
+    }
+  }
+
+  function getAllResources(): [ResourceType, number][] {
+    return ["purple", "orange", "green", "red", "blue", "yellow", "energy"].map(
+      (resourceType: ResourceType) => {
+        return [resourceType, this.getValue(resourceType)];
+      }
+    );
   }
 }
