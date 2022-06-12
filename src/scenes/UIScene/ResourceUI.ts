fd931bd981cbe52c39890f9a9ea6654ee75b04f5
thedaruma <rgiorda1@gmail.com>
2022-06-11 21:42:26 -0400

    refactor UI scene

--- src/scenes/UIScene/ResourceUI.ts
+++ src/scenes/UIScene/ResourceUI.ts
@@ -1,3 +1,15 @@
+import { ResourceType } from "./../../assets/data/stellar-bodies/Types";
+import { UIBar } from "../../components/UI/UIBar";
+import {
+  BLUE,
+  YELLOW,
+  RED,
+  WHITE,
+  ORANGE,
+  GREEN,
+  PURPLE,
+} from "../../utility/Constants";
+
 function buildBorder({
   x,
   y,
@@ -22,4 +34,104 @@ export function buildResourceBorder(scene: Phaser.Scene, x: number, y: number) {
     width: 150,
     height: 200,
   });
+}
+
+const UIBarData = [
+  {
+    resourceType: "blue",
+    colorHex: BLUE.hex,
+    position: { x: 0, y: 0 },
+  },
+  {
+    resourceType: "yellow",
+    colorHex: YELLOW.hex,
+    position: { x: 0, y: 50 },
+  },
+  {
+    resourceType: "red",
+    colorHex: RED.hex,
+    position: { x: 0, y: 100 },
+  },
+  {
+    resourceType: "orange",
+    colorHex: ORANGE.hex,
+    position: { x: 0, y: 0 },
+  },
+  {
+    resourceType: "green",
+    colorHex: GREEN.hex,
+    position: { x: 0, y: 50 },
+  },
+  {
+    resourceType: "purple",
+    colorHex: PURPLE.hex,
+    position: { x: 0, y: 100 },
+  },
+];
+
+export function buildResourceUI(scene: Phaser.Scene) {
+  const barDimensions = {
+    barWidth: 128,
+    barHeight: 42,
+  };
+  const contentMap = new Map<ResourceType, UIBar>();
+
+  const [
+    blueGasBar,
+    yellowGasBar,
+    redGasBar,
+    orangeMineralBar,
+    greenMineralBar,
+    purpleMineralBar,
+  ] = UIBarData.map(({ position, resourceType, colorHex }) => {
+    const bar = new UIBar({
+      scene,
+      position,
+      currentValue: 0,
+      maxValue: 10,
+      color: colorHex,
+      hasBackground: true,
+      ...barDimensions,
+    });
+    contentMap.set(resourceType as ResourceType, bar);
+
+    return bar;
+  });
+
+  const gasBorder = buildResourceBorder(scene, -75, -60);
+  const gasText = scene.add.text(-65, -55, "gas", {
+    fontFamily: "pixel",
+    fontSize: "25px",
+    color: WHITE.str,
+  });
+
+  const gasBarContainer = scene.add.container(0, 0, [
+    blueGasBar,
+    yellowGasBar,
+    redGasBar,
+    gasBorder,
+    gasText,
+  ]);
+
+  const mineralText = scene.add.text(-65, -55, "minerals", {
+    fontFamily: "pixel",
+    fontSize: "25px",
+    color: WHITE.str,
+  });
+  const mineralBorder = buildResourceBorder(scene, -75, -60);
+  const mineralBarContainer = scene.add.container(147, 0, [
+    orangeMineralBar,
+    greenMineralBar,
+    purpleMineralBar,
+    mineralBorder,
+    mineralText,
+  ]);
+
+  const resourceContainer = scene.add.container(
+    scene.game.canvas.width - 350,
+    -70,
+    [gasBarContainer, mineralBarContainer]
+  );
+
+  return { resourceContainer, contentMap };
 }
--- src/scenes/UIScene/UIScene.ts
+++ src/scenes/UIScene/UIScene.ts
@@ -1,30 +1,12 @@
 import { ResourceType } from "../../assets/data/stellar-bodies/Types";
 import { UIBar } from "../../components/UI/UIBar";
-import {
-  GREEN,
-  PURPLE,
-  ORANGE,
-  RED,
-  BLUE,
-  YELLOW,
-  WHITE,
-} from "../../utility/Constants";
+
 import DependentScene from "../DependentScene";
 import { StateResourceObject } from "../StateScene";
-import { buildResourceBorder } from "./ResourceUI";
+import { buildResourceUI } from "./ResourceUI";
 
 export class UIScene extends DependentScene {
-  private gasBarContainer: Phaser.GameObjects.Container;
-  private mineralBarContainer: Phaser.GameObjects.Container;
-  private resourceContainer: Phaser.GameObjects.Container;
   private UIParent: Phaser.GameObjects.Container;
-  private blueGasBar: UIBar;
-  private yellowGasBar: UIBar;
-  private redGasBar: UIBar;
-  private orangeMineralBar: UIBar;
-  private greenMineralBar: UIBar;
-  private purpleMineralBar: UIBar;
-
   public contentMap: Map<ResourceType, UIBar> = new Map();
   constructor() {
     super({
@@ -39,128 +21,16 @@ export class UIScene extends DependentScene {
 
   create(): void {
     this.buildUIParent();
-    this.buildResourceUI();
-    this.buildContentMap();
+    const { resourceContainer, contentMap } = buildResourceUI(this);
+    this.UIParent.add(resourceContainer);
+    this.contentMap = contentMap;
+
     this.setSceneListeners();
   }
 
+  /** Where all of the UI on the bottom of the screen lives */
   private buildUIParent() {
     this.UIParent = this.add.container(60, this.game.canvas.height - 100);
-  }
-
-  private buildResourceUI() {
-    const barDimensions = {
-      barWidth: 128,
-      barHeight: 42,
-    };
-    this.blueGasBar = new UIBar({
-      scene: this,
-      position: { x: 0, y: 0 },
-      currentValue: 0,
-      maxValue: 10,
-      color: BLUE.hex,
-      hasBackground: true,
-      ...barDimensions,
-    });
-    this.yellowGasBar = new UIBar({
-      scene: this,
-      position: { x: 0, y: 50 },
-      currentValue: 0,
-      maxValue: 10,
-      color: YELLOW.hex,
-      hasBackground: true,
-      ...barDimensions,
-    });
-    this.redGasBar = new UIBar({
-      scene: this,
-      position: { x: 0, y: 100 },
-      currentValue: 0,
-      maxValue: 10,
-      color: RED.hex,
-      hasBackground: true,
-      ...barDimensions,
-    });
-
-    const gasBorder = buildResourceBorder(this, -75, -60);
-
-    const gasText = this.add.text(-65, -55, "gas", {
-      fontFamily: "pixel",
-      fontSize: "25px",
-      color: WHITE.str,
-    });
-
-    this.gasBarContainer = this.add.container(0, 0, [
-      this.blueGasBar,
-      this.yellowGasBar,
-      this.redGasBar,
-      gasBorder,
-      gasText,
-    ]);
-
-    this.orangeMineralBar = new UIBar({
-      scene: this,
-      position: { x: 0, y: 50 },
-      currentValue: 0,
-      maxValue: 10,
-      color: ORANGE.hex,
-      hasBackground: true,
-      ...barDimensions,
-    });
-    this.greenMineralBar = new UIBar({
-      scene: this,
-      position: { x: 0, y: 100 },
-      currentValue: 0,
-      maxValue: 10,
-      color: GREEN.hex,
-      hasBackground: true,
-      ...barDimensions,
-    });
-    this.purpleMineralBar = new UIBar({
-      scene: this,
-      position: { x: 0, y: 0 },
-      currentValue: 0,
-      maxValue: 10,
-      color: PURPLE.hex,
-      hasBackground: true,
-      ...barDimensions,
-    });
-
-    const mineralText = this.add.text(-65, -55, "minerals", {
-      fontFamily: "pixel",
-      fontSize: "25px",
-      color: WHITE.str,
-    });
-    const mineralBorder = buildResourceBorder(this, -75, -60);
-    this.mineralBarContainer = this.add.container(147, 0, [
-      this.orangeMineralBar,
-      this.greenMineralBar,
-      this.purpleMineralBar,
-      mineralBorder,
-      mineralText,
-    ]);
-
-    this.resourceContainer = this.add.container(
-      this.game.canvas.width - 350,
-      -70,
-      [this.gasBarContainer, this.mineralBarContainer]
-    );
-
-    this.UIParent.add(this.resourceContainer);
-  }
-
-  /** Map the ui displays to their content type property */
-  private buildContentMap() {
-    [
-      ,
-      ["orange", this.orangeMineralBar],
-      ["purple", this.purpleMineralBar],
-      ["red", this.redGasBar],
-      ["yellow", this.yellowGasBar],
-      ["green", this.greenMineralBar],
-      ["blue", this.blueGasBar],
-    ].forEach(([contentType, uiBar]) =>
-      this.contentMap.set(contentType as ResourceType, uiBar as UIBar)
-    );
   }
 
   private setSceneListeners() {
